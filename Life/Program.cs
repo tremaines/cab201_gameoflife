82276b17c2fe46279c3a7a89d0b77dc1c90900c9
tremaines <tremaine.stroebel@gmail.com>
2020-09-02 18:41:27 +1000

    Initial commit

---
+++ Life/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Display;
+
+namespace Life
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("You have not provided any arguments, reverting to defaults!");
+                return;
+            }
+            else
+            {
+                List<List<string>> userArgs = ParseArgs(args);
+
+                CheckArgs(userArgs);
+
+                /*for (int i = 0; i < userArgs.Count; i++)
+                {
+                    for (int j = 0; j < userArgs[i].Count; j++)
+                    {
+                        Console.WriteLine(userArgs[i][j]);
+                    }
+                    Console.WriteLine("<------------>");
+                }*/
+
+            }
+
+        }
+
+        /// <summary>
+        /// Parses a list of strings to group options (beginning with "--") with their parameters
+        /// as entered by the user
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns>
+        /// A string type list of lists. Each sublist comprises of at least an option
+        /// and any parameters if entered.
+        /// </returns>
+        public static List<List<string>> ParseArgs(string[] args)
+        {
+
+            List<List<string>> arguments = new List<List<string>>();
+            
+            bool atLeastOneOption = false;  //  Set to true if there is at least one seemingly valid option
+            int firstValidOption = 0;       //  Set to the index of the first seemingly valid option
+            int counter = 0;                //  Counter for while loop
+           
+            while (!atLeastOneOption)
+            {
+                if (!args[counter].StartsWith("--"))
+                {
+                    Console.WriteLine($"Argument {args[counter]} has been ignored: ");
+                    Console.WriteLine($"\t- Precede all arguments with '--', e.g. '--{args[counter]}' followed by the parameters.");
+                    Console.WriteLine("\t- Refer to README.md for all valid arguments and more details instructions.");
+                }
+                else
+                {
+                    atLeastOneOption = true; // We now know there is at least one valid argument we can use
+                    firstValidOption = counter;
+                    break;
+                }
+                
+                counter++;
+                //  If we reach the end of the array and there haven't been any valid options, return the empty list and tell the user
+                //  the defaults will be used
+                if (counter == args.Length)
+                {
+                    Console.WriteLine("\nThere are no valid arguments, reverting to default values for this Game of Life!");
+                    return arguments;
+                }
+            }
+
+            //  Start grouping options and parameters into individual lists, starting from the first valid option
+            for (int i = firstValidOption; i < args.Length; i++)
+            {
+                List<string> optsAndParams = new List<string>();
+                int j = i + 1;
+                //  Add the option to the first index of the new list
+                optsAndParams.Add(args[i]);
+                if (j < args.Length)
+                {
+                    //  Keep adding parameters to the new list until we reach the next option OR the end of the args array
+                    while ((j < args.Length) && (!args[j].StartsWith("--")))
+                    {
+                        optsAndParams.Add(args[j]);
+                        //  Set i to j so the loop counts from the current index rather than the index of the most recent option
+                        i = j;
+                        j++;
+                    }
+                }
+                //  Once we have reached the next option or the end of the args array, add the list to the arguments list
+                arguments.Add(optsAndParams);
+            }
+
+            return arguments;
+        }
+
+        /// <summary>
+        /// Checks each sublist of options and parameters and calls the relevant methods for those
+        /// </summary>
+        /// <param name="args"></param>
+        public static void CheckArgs(List<List<string>> args)
+        {
+            args.ForEach(delegate (List<string> options)
+            {
+                switch (options[0])
+                {
+                    case "--dimensions":
+                        Console.WriteLine($"1 - {options[0]}");
+                        Settings.Dimensions(options);
+                        break;
+                    case "--periodic":
+                        Console.WriteLine($"2 - {options[0]}");
+                        Settings.Periodic(options);
+                        break;
+                    case "--random":
+                        Console.WriteLine($"3 - {options[0]}");
+                        Settings.RandomFactor(options);
+                        break;
+                    case "--seed":
+                        Console.WriteLine($"4 - {options[0]}");
+                        Settings.OpenSeed(options);
+                        break;
+                    case "--generations":
+                        Console.WriteLine($"5 - {options[0]}");
+                        Settings.ChangeGenerations(options);
+                        break;
+                    case "--max-update":
+                        Console.WriteLine($"6 - {options[0]}");
+                        break;
+                    case "--step":
+                        Console.WriteLine($"7 - {options[0]}");
+                        break;
+                    default:
+                        Console.WriteLine($"Unknown argument '{options[0]}'.");
+                        break;
+                }
+            });
+        }
+    }
+}
---
+++ Life/Settings.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Life
+{
+    /// <summary>
+    /// This class holds the settings for the Game of Life board
+    /// Defaults are initialised at the beginning of the class and the various methods
+    /// change those defaults to user define values (assuming the user's input it valid!)
+    /// </summary>
+    /// <author>Tremaine Stroebel</author>
+    /// <date>August 2020</date>
+    class Settings
+    {
+        //  Initialise game attributes with default values
+        private static int rows = 16, columns = 16;
+        private static bool periodic = false;
+        private static float random = 0.5F;
+        private static string seedFile = "";
+        private static int generations = 50;
+        private static float updateRate = 5;
+        private static bool stepMode = false;
+
+
+        private static bool CheckNumOfArgs(List<string> args, int numExpected)
+        {
+            string argument = args[0];
+            bool rightNumOfArgs = false;
+            
+            if (args.Count != numExpected)
+            {
+                Console.WriteLine($"The argument '{argument}' requires {numExpected - 1} parameters.");
+            }
+            else
+            {
+                rightNumOfArgs = true;
+            }
+
+            return rightNumOfArgs;
+        }
+
+        /// <summary>
+        /// Changes dimensions of board based on valid user input
+        /// </summary>
+        /// <param name="userInput"></param>
+        public static void Dimensions(List<String> userInput)
+        {
+            int userRows;
+            int userCols;
+            int numExpectedArgs = 3;
+            bool changeDefaults = false;
+
+            //  Check user has entered two parameters (one for rows and one for columns)
+            //  Should be three (including the 0 index "--dimensions" argument)
+            if (CheckNumOfArgs(userInput, numExpectedArgs))
+            {
+                if ((Int32.TryParse(userInput[1], out userRows) && Int32.TryParse(userInput[2], out userCols))
+                    && ((userRows >= 4 && userRows <= 48) && (userCols >= 4 && userCols <= 48)))
+                {
+                    rows = userRows;
+                    columns = userCols;
+                    changeDefaults = true;
+                }
+                else
+                {
+                    Console.WriteLine("--dimensions requires two positive integers between 4 and 48 inclusive.");
+                }
+            }
+            
+            if (!changeDefaults)
+            {
+                Console.WriteLine("Using default dimensions of 16 rows and 16 columns due to invalid user input.");
+            }
+        }
+
+        /// <summary>
+        /// If the user has entered --periodic, will change the behaviour type to be periodic.
+        /// Does not take any parameters so they will be ignored and the user will be told as such
+        /// </summary>
+        /// <param name="userInput"></param>
+        public static void Periodic(List<string> userInput)
+        {
+            if (userInput.Count > 1)
+            {
+                Console.Write("Unknow parameter(s) for --periodic: ");
+                for (int i = 1; i < userInput.Count; i++)
+                {
+                    if (i + 1 == userInput.Count)
+                    {
+                        Console.WriteLine($"'{userInput[i]}'");
+                    }
+                    else
+                    {
+                        Console.Write($"'{userInput[i]}', ");
+                    }
+                }
+                Console.WriteLine("--periodic does not require parameters");
+            }
+
+            periodic = true;
+        }
+
+        /// <summary>
+        /// If --random is called, validates the user's input and changes the value of the random variable if OK
+        /// </summary>
+        /// <param name="userInput"></param>
+        public static void RandomFactor(List<string> userInput)
+        {
+            float userRand;
+            int numExpectedArgs = 2;
+
+            if (CheckNumOfArgs(userInput, numExpectedArgs))
+            {
+                if ((float.TryParse(userInput[1], out userRand)) && (userRand >= 0 && userRand <= 1))
+                {
+                    random = userRand;
+                }
+                else
+                {
+                    Console.WriteLine("--random requires a float parameter between 0 and 1 inclusive.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Using default value of 50%");
+            }
+        }
+
+        /// <summary>
+        /// Open and read seed file ****************TO DO**********TO DO***********TO DO*********************
+        /// </summary>
+        /// <param name="options"></param>
+        public static void OpenSeed(List<string> options)
+        {
+            return;
+        }
+
+        /// <summary>
+        /// If the --generation argument is called, validates user input and changes number of generations run
+        /// </summary>
+        /// <param name="userInput"></param>
+        public static void ChangeGenerations(List<string> userInput)
+        {
+            int numOfExpectedArgs = 2;
+            int userGens;
+
+            if (CheckNumOfArgs(userInput, numOfExpectedArgs))
+            {
+                if (Int32.TryParse(userInput[1], out userGens) && userGens > 0)
+                {
+                    generations = userGens;
+                }
+                else
+                {
+                    Console.WriteLine("Number of generations must be a positive, non-zero integer.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Using default value of 50");
+            }
+                
+        }
+
+        public static void ChangeMaxUPS(List<string> userInput)
+        {
+            int numOfExpectedArgs = 2;
+            float userUPS;
+
+            if (CheckNumOfArgs(userInput, numOfExpectedArgs))
+            {
+                if (float.TryParse(userInput[1], out userUPS) && (userUPS >= 1 && userUPS <= 30))
+                {
+                    updateRate = userUPS;
+                }
+                else
+                {
+                    Console.WriteLine("Number of updates per second must be a float between 1 and 30 inclusive.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Using default value of 5 updates per second.");
+            }
+        }
+    }
+}
